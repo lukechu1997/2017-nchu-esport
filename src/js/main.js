42c30c4631a9863a8dfc7f158a1e49a550d56c79
Luke Chu <lukechu421@gmail.com>
2017-10-19 00:05:28 +0800

    init

---
+++ src/js/main.js
@@ -0,0 +1,66 @@
+$(document).ready( () => {
+  var page = 0
+  $('.page0').show()
+  $('.btn1').click( () => {
+    $('.page'+page).hide()
+    $('.page1').show()
+    page = 1;
+  })
+  $('.btn2').click( () => {
+    $('.page'+page).hide()
+    $('.page2').show()
+    page = 2;
+  })
+  $('.btn3').click( () => {
+    $('.page'+page).hide()
+    $('.page3').show()
+    page = 3;
+  })
+  $('.btn4').click( () => {
+    $('.page'+page).hide()
+    $('.page4').show()
+    page = 4;
+  })
+  $('.btn5').click( () => {
+    $('.page'+page).hide()
+    $('.page5').show()
+    page = 5;
+  })
+  $('.btn6').click( () => {
+    $('.page'+page).hide()
+    $('.page6').show()
+    page = 6;
+  })
+  $('.btn7').click( () => {
+    $('.page'+page).hide()
+    $('.page7').show()
+    page = 7;
+  })
+  $('.btn8').click( () => {
+    $('.page'+page).hide()
+    $('.page8').show()
+    page = 8;
+  })
+  $('.btn9').click( () => {
+    $('.page'+page).hide()
+    $('.page9').show()
+    page = 9;
+  })
+  $('.btn10').click( () => {
+    $('.page'+page).hide()
+    $('.page10').show()
+    page = 10;
+  })
+  $('.single').click( () => {
+    $('.multiForm').hide()
+    $('.singleForm').show()
+  })
+  $('.multi').click( () => {
+    $('.singleForm').hide()
+    $('.multiForm').show()
+  })
+  $('.dropbtn').click( () => {
+    $('.dropdown-content').toggle("slow")
+    $('.dropbtn').css({"opacity": "1"})
+  })
+})
